4a7d489ff4887672f64f134fc9c0b3723d626902
oupson <oupson1er@gmail.com>
2022-10-10 11:03:31 +0200

    Fix compilation on other target than linux

--- rustcryptfs-fuse/src/lib.rs
+++ rustcryptfs-fuse/src/lib.rs
@@ -1,6 +1,10 @@
+#[cfg(target_os = "linux")]
 mod encrypted_filesystem;
+#[cfg(target_os = "linux")]
 mod inode_cache;
 
+#[cfg(target_os = "linux")]
 pub mod error;
 
+#[cfg(target_os = "linux")]
 pub use encrypted_filesystem::EncryptedFs;
--- rustcryptfs-mount/src/lib.rs
+++ rustcryptfs-mount/src/lib.rs
@@ -1,12 +1,12 @@
 use std::path::Path;
 
-use rustcryptfs_fuse::EncryptedFs;
-
 #[cfg(target_os = "linux")]
 pub fn mount<P>(path: P, mount_point: P, password: &str) -> rustcryptfs_fuse::error::Result<()>
 where
     P: AsRef<Path>,
 {
+    use rustcryptfs_fuse::EncryptedFs;
+
     let fs = EncryptedFs::new(path, password)?;
 
     fs.mount(mount_point)?;
